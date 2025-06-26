39d2111d8ef2b0454cf214b110e15f5d05a2b91c
miratpc <mira_tpc@hotmail.com>
2025-06-26 16:24:27 +0000

    chore: update Yandex.py with edge generation for emj dataset

--- data_preprocess/Yandex.py
+++ data_preprocess/Yandex.py
@@ -14,6 +14,7 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import *
 from math import log
 import random
